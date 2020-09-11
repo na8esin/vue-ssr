31fb19dee8836bef3d2aecfc72ea4b321df8208a
na8esin <na8esin.mpst@gmail.com>
2020-09-11 09:17:20 +0900

    reference : https://vuejs.org/v2/guide/single-file-components.html

--- App.vue
+++ App.vue
@@ -1,3 +1,20 @@
 <template>
-  <div>tommorrow</div>
+  <p>{{ greeting }} World!</p>
 </template>
+
+<script>
+module.exports = {
+  data: function() {
+    return {
+      greeting: "Hello"
+    };
+  }
+};
+</script>
+
+<style scoped>
+p {
+  font-size: 2em;
+  text-align: center;
+}
+</style>
